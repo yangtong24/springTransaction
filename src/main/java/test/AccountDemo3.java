8eabb6e6158c00a17aeefd559feeddeebf7975b0
yt <834969700@qq.com>
2017-09-24 15:40:56 +0800

    修复路径

---
+++ src/main/java/test/AccountDemo3.java
@@ -0,0 +1,29 @@
+package test;
+
+import com.ytcool.service.AccountService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * 声明式事务管理的方式测试类
+ */
+@ContextConfiguration(locations="classpath:applicationContext2.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+public class AccountDemo2 {
+
+    //@Resource(name = "service2")
+    //注入代理类,因为代理类进行的增强操作
+    @Resource(name = "accountServiceProxy")
+    private AccountService accountService;
+
+
+    //测试业务层的类
+    @Test
+    public void demo(){
+        accountService.transfer("aaa", "bbb", 200d);
+    }
+}
